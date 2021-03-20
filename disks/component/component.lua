b0717072626a6c514a0b4fc635de9e6b1b796195
JonathanCline <59320680+JonathanCline@users.noreply.github.com>
2021-03-20 04:48:51 -0500

    r

---
+++ disks/component/component.lua
@@ -0,0 +1,20 @@
+
+local libString = [[
+local component = component
+
+component.all = function(name)
+    local out = {}
+    local iter = component.list(name)
+    local comp = iter()
+    while comp do
+        out[#out+1] = comp
+        comp = iter()
+    end
+    return comp
+end
+
+return component
+]]
+
+local inslib = require("insLib.lua")
+inslib.add_library("component.lua", libString)
---
+++ disks/graphics/gfx.lua
@@ -0,0 +1,31 @@
+local libstr = [[
+local component = require("component.lua")
+
+GFXDisplayList = nil
+GFXDisplayList = {}
+do
+
+    local gpu_iter = component.list("gpu")
+    local screen_iter = component.list("screen")
+
+    local gpu = gpu_iter()
+
+    while gpu do
+        local display =
+        {
+            gpu = gpu,
+            screen = screen_iter()
+        }
+        if display.screen then
+            component.invoke(display.gpu, "bind", display.screen, true)
+            component.invoke(display.gpu, "set", 1, 1, tostring(#GFXDisplayList+1))
+        end
+        GFXDisplayList[#GFXDisplayList+1] = display
+        gpu = gpu_iter()
+    end
+
+end
+]]
+
+local insLib = require("insLib.lua")
+insLib.add_boot("01_gfx.lua", libstr)
